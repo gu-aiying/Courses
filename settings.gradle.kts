8d1c88141bbab3163335eea334f140068aafc249
gu_ai <gu_aiying@mail.ru>
2025-10-11 22:41:01 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Courses"
+include(":app")
+include(":data")
+include(":domain")
+include(":presentation-auth")
+include(":presentation-main")
+include(":presentation-favorites")
